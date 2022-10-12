6d078eb7bfb9abc122610841751a783455df91da
u.sadullaev <u.sadullaev@itransition.com>
2022-10-12 21:36:09 +0500

    Add test

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+/* eslint-disable @angular-eslint/template/no-negated-async */
+import { Component } from "@angular/core";
 import { LoaderService } from "../shared/loader/loader.service";
 
 @Component({
@@ -12,7 +12,7 @@ import { LoaderService } from "../shared/loader/loader.service";
         *ngIf="loaderService.loader | async"
       ></mat-spinner>
 
-      <div id="lists-wrapper" *ngIf="(loaderService.loader | async) === true">
+      <div id="lists-wrapper" *ngIf="!(loaderService.loader | async)">
         <router-outlet></router-outlet>
       </div>
     </div>
